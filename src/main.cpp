d0b7d0750398ab1d86d98bb5eb9121afe7938658
Rogery Vargas <35406868+rogery1999@users.noreply.github.com>
2023-12-20 16:05:47 -0500

    Calculate two times input (#1)
    * feat: double the input number added
    
    * feat: ask for input 2 number and calculate the addition and sustraction of them

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,20 @@
 #include <iostream>
-#include <vector>
-#include <string>
-#include "config.h"
-#include "MathFunctions/MathFunctions.h"
-
-void doSomething(int &)
-{
-}
 
 int main()
 {
-  int x;
-  doSomething(x);
 
-  std::cout << "uninitialized value of x: " << x << '\n';
+  std::cout << "Enter an integer: ";
 
-  std::cout << "Enter a number for x: ";
-  std::cin >> x;
+  int inputNum{};
+  std::cin >> inputNum;
 
-  std::cout << "the value of x enter by the user is: " << x << '\n';
+  std::cout << "Enter another integer: ";
+
+  int inputNum2{};
+  std::cin >> inputNum2;
+
+  std::cout << inputNum << " + " << inputNum2 << " is " << inputNum + inputNum2 << '\n';
+  std::cout << inputNum << " - " << inputNum2 << " is " << inputNum - inputNum2 << '\n';
 
   return 0;
 }
